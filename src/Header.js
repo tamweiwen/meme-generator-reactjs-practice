a2679a75a672f766b3e1775a71f30869fafbe7fe
tamweiwen <20170012034@my.xu.edu.ph>
2022-09-23 13:40:47 +0800

    first progress

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+
+
+export default function() {
+    return(
+        <div className="header">
+            <div className="logo-title">
+                <img src="../images/troll-face.png" alt="Troll Face" className="trollface" />
+                <h3 className="title">Meme Generator</h3>
+            </div>
+            <h2 className="projectname">
+                React Course - Project 3
+            </h2>
+        </div>
+    )
+}
---
+++ src/Main.js
@@ -0,0 +1,13 @@
+
+
+export default function Main() {
+    return(
+        <div className="main">
+            <div className="input">
+                <input type="text" className="uppertext" placeholder="Upper-text"/>
+                <input type="text" className="bottomtext" placeholder="Bottom-text"/>
+            </div>
+            <p className="getImg-btn">Get a new meme image 🖼️</p>
+        </div>
+    )
+}
